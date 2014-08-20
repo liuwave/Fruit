0dad4f2b429203e88f681f386a791d86ef764c5d
liuwave <liuwave@qq.com>
2014-08-20 10:03:44 +0800

    Signed-off-by: liuwave <liuwave@qq.com>

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用入口文件
+
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+//define('BIND_MODULE','Admin');
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+//define('BUILD_LITE_FILE',true);
+// 定义应用目录
+define('APP_PATH','./Application/');
+
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
+//require './ThinkPHP/Mode/sae.php';//加载SAE引擎文件
+
+
+// 亲^_^ 后面不需要任何代码了 就是如此简单
+
+// 定义存储类型和应用模式为SAE（用于支持SAE平台）
+//define('STORAGE_TYPE','sae');
+//define('APP_MODE','sae');
+//define('APP_PATH','./Application/');
+//require './Application/Runtime/lite.php';
